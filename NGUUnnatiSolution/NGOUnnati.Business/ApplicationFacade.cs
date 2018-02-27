6147db0fd114fa73ed60fd99d25d1df5ef62a127
Vikas Singh <vsvikassingh49@gmail.com>
2018-02-28 00:28:25 +0530

    Added Source Code

---
+++ NGUUnnatiSolution/NGOUnnati.Business/ApplicationFacade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NGOUnnati.BusinessObject;
+
+namespace NGOUnnati.Business
+{
+    public class ApplicationFacade : IApplicationFacade
+    {
+
+    }
+}
